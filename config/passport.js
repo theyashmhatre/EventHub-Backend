19c9ccd05dd5f1e1120a20cb9dbd7ab4a776e229
yashmhatrezt <60573218+theyashmhatre@users.noreply.github.com>
2023-04-29 17:56:07 +0530

    first commit, initial setup

---
+++ config/passport.js
@@ -0,0 +1,39 @@
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const mysqlConnection = require("../config/dbConnection");
+
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = process.env.secretOrKey;
+opts.algorithms = ['HS256'];
+
+module.exports = passport => {
+
+    passport.use(
+        new JwtStrategy(opts, (jwt_payload, done) => {
+            mysqlConnection.query(`SELECT * from user where username = ${jwt_payload.username}`, function (err, row, fields) {
+
+                if (err) done(null, false);  //returns unauthorized 404 code
+
+                else {
+                    const user = row[0];
+                    if (user) {
+
+                        return done(null, user);
+                    }
+                    return done(null, false);
+                }
+            });
+        })
+
+    );
+
+    passport.serializeUser((user, done) => {
+        done(null, user);
+    });
+
+    passport.deserializeUser((user, done) => {
+        done(null, user);
+    });
+};
---
+++ server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 8000;
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
+
+
+// Passport Middleware
+app.use(passport.initialize());
+
+//Passport config
+require("./config/passport")(passport);
+
+// Function to serve all static files
+// inside public directory.
+app.use(express.static('assets'));
+
+app.use('*', (req, res) => {
+    res.status(404).json({ msg: 'Not Found' });
+});
+
+app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
